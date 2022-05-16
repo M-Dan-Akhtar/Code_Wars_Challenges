cbdd8f61ab0ae12a03a6a3d2d1f30320ac5ef8c2
Toxikr3 <Toxikr3@hotmail.com>
2022-05-15 20:36:58 -0400

    Daily Challenge - Count the Smiley Faces

---
+++ Count_the_Smiley_Faces.cpp
@@ -0,0 +1,37 @@
+/*
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+*/
+
+#include <regex>
+
+int countSmileys(std::vector<std::string> arr)
+{
+  size_t count = 0;
+  
+  if(arr.empty())
+    return count;
+   
+  std::regex reg("(:|;)(-|~)?(\\)|D)");
+  for(auto s : arr)
+  {
+    if(std::regex_match(s,reg))
+      count++;
+  }
+  
+  return count;
+}
