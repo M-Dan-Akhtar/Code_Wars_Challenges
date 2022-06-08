f976c794d4dab77e83e4b2b9bb3d4a5cac0734a0
Toxikr3 <Toxikr3@hotmail.com>
2022-06-07 20:54:17 -0400

    Daily Challenge - Count characters in your string

---
+++ Count_characters_in_your_string.cpp
@@ -0,0 +1,18 @@
+/*
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+*/
+
+#include <map>
+#include <string>
+
+std::map<char, unsigned> count(const std::string& string) {
+  
+  std::map<char, unsigned> m;
+  
+  for(auto c : string)
+    m[c]++;
+  
+  return m;
+}
